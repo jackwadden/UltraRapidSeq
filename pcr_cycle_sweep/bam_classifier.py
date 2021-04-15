40bdea4fadfee215403a57953eeb6d9a6735dae6
jackwadden <jackwadden@gmail.com>
2021-04-15 09:14:47 -0400

    Updated bam classifier to add basecall to classification. Verified to match compute_vaf_over_time for 14min_e2e dataset.

--- pcr_cycle_sweep/bam_classifier.py
+++ pcr_cycle_sweep/bam_classifier.py
@@ -2,7 +2,20 @@ import sys, os, argparse, collections
 
 import pysam
 import vcfpy
+##########
+def rc(base):
 
+    if base == 'A':
+        return 'T'
+    elif base =='T':
+        return 'A'
+    elif base == 'C':
+        return 'G'
+    elif base == 'G':
+        return 'C'
+    else:
+        return ''
+    
 
 ##########
 def is_in_target_region(vcf_record, entry):
@@ -46,17 +59,47 @@ for record in vcf_reader:
 
 vcf_chrom = vcf_record.CHROM
 vcf_locus = vcf_record.POS
-
-print(vcf_chrom, vcf_locus)
+wt_base = vcf_record.REF
+mut_base = vcf_record.ALT[0].value
 
 # for each entry, categorize as "target, fragment, background, unmapped"
 read_classification_list = list()
 read_classification_counters = {'target' : 0, 'fragment': 0, 'background' : 0, 'unmapped' : 0}
 
+mut_counter = 0
+wt_counter = 0
+C_counter = 0
+G_counter = 0
+
+# collect map of read_ids to base according to ref position
+pileup_iter = bamfile.pileup(vcf_record.CHROM, vcf_record.POS - 1, vcf_record.POS,
+                             truncate=True,
+                             stepper="samtools",
+                             min_base_quality=0,
+                             ignore_overlaps=False,
+                             ignore_orphans=False)
+
+read_id_to_base_map = dict()
+
+for column in pileup_iter:
+    if(column.reference_pos == vcf_record.POS - 1):
+        for read in column.pileups:
+            if read.is_del:
+                read_id_to_base_map[read.alignment.query_name] = 'del'
+            elif read.is_refskip:
+                read_id_to_base_map[read.alignment.query_name] = 'ins'
+            else:
+                read_id_to_base_map[read.alignment.query_name] = read.alignment.query_sequence[read.query_position]
+bamfile.close()
+
+# parse bamfile again
+bamfile = pysam.AlignmentFile(bam_fn, 'rb')
+
+# attach read id, classification ,and base into one tuple
 for entry in bamfile:
-    #print(entry.query_name, entry.reference_name)
 
     classification = ''
+    call_at_locus = ''
 
     if entry.is_unmapped:
         classification = 'unmapped'
@@ -72,16 +115,29 @@ for entry in bamfile:
         else:
             classification = 'background'
 
-    read_classification_list.append((entry.query_name, classification))
+    #print(entry.query_name, classification, read_id_to_base_map[entry.query_name])
+    if entry.query_name not in read_id_to_base_map.keys():
+        base = 'X'
+    else:
+        base = read_id_to_base_map[entry.query_name]
+        
+    if base == mut_base:
+        mut_counter = mut_counter + 1
+    elif base == wt_base:
+        wt_counter = wt_counter + 1
+    
+    read_classification_list.append((entry.query_name, classification,base))
     read_classification_counters[classification] += 1
     
-#for name, classification in read_classification_list:
-#    print("{},{}".format(name, classification))
+for name, classification,base in read_classification_list:
+    print("{},{},{}".format(name, classification,base))
+
+print(mut_counter, wt_counter)
 
 # write classifications to file
 with open('bam_classifications.csv', 'w') as filehandle:
-    filehandle.writelines("{},{}".format(name, classification) for name,classification in read_classification_list)
+    filehandle.writelines("{},{},{}\n".format(name, classification, base) for name,classification,base in read_classification_list)
         
 
 # print summary of classifications
-print(read_classification_counters)
+#print(read_classification_counters)
